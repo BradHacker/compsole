79d545b0ebc1dc7672642061af913c107c133ee1
BradHacker <harkerbd@gmail.com>
2025-02-10 01:14:22 -0500

    Remove base from Vite config

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -4,13 +4,5 @@ import viteTsconfigPaths from 'vite-tsconfig-paths'
 import codegen from 'vite-plugin-graphql-codegen'
 
 export default defineConfig({
-  // depending on your application, base can also be "/"
-  base: '',
   plugins: [react(), viteTsconfigPaths(), { ...codegen(), apply: 'serve' }],
-  server: {
-    // this ensures that the browser opens upon server start
-    open: true,
-    // this sets a default port to 3000
-    port: 3000,
-  },
 })
